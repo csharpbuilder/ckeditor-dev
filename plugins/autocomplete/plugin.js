beacbc0e4d15ff179150ee23ca41767aa75b86ce
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-06-11 15:58:48 +0200

    Added following space option.

--- plugins/autocomplete/plugin.js
+++ plugins/autocomplete/plugin.js
@@ -172,6 +172,13 @@
 		this.throttle = config.throttle !== undefined ? config.throttle : 20;
 
 		/**
+		 * See {@link CKEDITOR.plugins.autocomplete.configDefinition#followingSpace}.
+		 *
+		 * @property {Boolean} [followingSpace]
+		 */
+		this.throttle = config.followingSpace !== undefined ? config.followingSpace : true;
+
+		/**
 		 * The autocomplete view instance.
 		 *
 		 * @readonly
@@ -326,11 +333,17 @@
 			}
 
 			var item = this.model.getItemById( itemId ),
-				editor = this.editor;
+				editor = this.editor,
+				selection = editor.getSelection();
 
 			editor.fire( 'saveSnapshot' );
-			editor.getSelection().selectRanges( [ this.model.range ] );
+			selection.selectRanges( [ this.model.range ] );
 			editor.insertHtml( this.getHtmlToInsert( item ), 'text' );
+
+			if ( this.followingSpace ) {
+				insertSpaceAfterMatch( editor );
+			}
+
 			editor.fire( 'saveSnapshot' );
 		},
 
@@ -1241,6 +1254,22 @@
 		return editor.window.getFrame().getParent();
 	}
 
+	function insertSpaceAfterMatch( editor ) {
+		var selection = editor.getSelection();
+
+		var nextNode = selection.getRanges()[ 0 ].getNextNode( function( node ) {
+			return Boolean( node.type == CKEDITOR.NODE_TEXT && node.getText() );
+		} );
+
+		if ( nextNode && nextNode.getText().match( /^\s+/ ) ) {
+			var range = editor.createRange();
+			range.setStart( nextNode, 1 );
+			selection.selectRanges( [ range ] );
+		} else {
+			editor.insertHtml( '&nbsp;' );
+		}
+	}
+
 	/**
 	 * Abstract class describing the definition of a {@link CKEDITOR.plugins.autocomplete autocomplete} plugin configuration.
 	 *
@@ -1336,6 +1365,12 @@
 	 * Indicates throttle threshold expressed in milliseconds reducing text checks frequency.
 	 *
 	 * @property {Number} [throttle=20]
+	 */
+
+	/**
+	 * Indicates if a following space should be added after accepted match.
+	 *
+	 * @property {Number} [followingSpace=true]
 	 */
 
 	/**
